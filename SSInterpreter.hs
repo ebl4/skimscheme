50adea988ebc294366bd3ccb8bb921dbee2cabc5
fulano7 <irvm@cin.ufpe.br>
2015-01-27 20:48:34 -0300

    if-else

--- SSInterpreter.hs
+++ SSInterpreter.hs
@@ -1,4 +1,4 @@
-{-
+﻿{-
 
 A basic interpreter for a purely functional subset of Scheme named SkimScheme.
 Part of this interpreter has been derived from the "Write Yourself a Scheme in
@@ -45,20 +45,51 @@ eval env val@(Atom var) = stateLookup env var
 eval env val@(Number _) = return val
 eval env val@(Bool _) = return val
 eval env (List [Atom "quote", val]) = return val
+eval env (List [Atom "if", pred, conseq, alt]) = 
+        do result <- eval env pred
+           case result of
+               Bool False -> eval env alt
+               otherwise -> eval env conseq
 eval env (List (Atom "begin":[v])) = eval env v
-eval env (List (Atom "begin": l1: ls)) = (eval env l1) >>= (\v -> case v of { (error@(Error _)) -> return error; otherwise -> eval env (List (Atom "begin": ls))})
+eval env (List (Atom "begin": l: ls)) = (eval env l) >>= (\v -> case v of { (error@(Error _)) -> return error; otherwise -> eval env (List (Atom "begin": ls))})
 eval env (List (Atom "begin":[])) = return (List [])
 eval env lam@(List (Atom "lambda":(List formals):body:[])) = return lam
---eval env clo@(List (Atom "make-closure":(Atom "lambda":(List formals):body:[]))) = return 
+
+--eval env clo@(List (Atom "make-closure":(Atom "lambda":(List formals):body:[]))) = return (List (Atom "make-closure":(Atom "lambda":(List (formals ++ evalBack env 0 (size env))):body:[])))  
 
 -- fazer lookup "let" em env, buscar var em formals; usar >>=
+ 
 -- evalBack env "let" >>= (\v -> case v of { (error@(Error _)) -> return clo; otherwise -> eval env (List (Atom "lambda":(List formals):body:[]))})
+
 
 -- The following line is slightly more complex because we are addressing the
 -- case where define is redefined by the user (whatever is the user's reason
 -- for doing so. The problem is that redefining define does not have
 -- the same semantics as redefining other functions, since define is not
 -- stored as a regular function because of its return type.
+
+eval env (List [Atom "let", List escope, body]) = 
+  ST (\s e -> let
+                (ST f) = applyLet env escope body  
+                (res, newStateS, newStateE) = f s e              
+                listVars = [var | List [var, val] <- escope ]  
+                boolNotCopies = (length listVars) == (length (removeCopies listVars))
+            in if (not boolNotCopies) then (res, newStateS, e)
+               else (Error ("let tem copias de variaveis locais"), s, e)
+      )
+
+
+eval env lis@(List [Atom "make-closure2", lambda]) = -- testei com lambda com o padrao (Atom escope, body) e deu erro; ebl4
+  ST (\s e -> let
+                (ST f) = eval env lambda 
+                -- (ST f) = eval env escope body  
+                (res, newStateS, newStateE) = f s e              
+                -- listVars = [var | List [var, val] <- escope ]  
+                -- boolNotCopies = (length listVars) == (length (removeCopies listVars))
+            in (res, newStateS, newStateE) -- if (not boolNotCopies) then (res, newStateS, newStateE) -- neste caso, a make-closure devolve o ambiente local modificado de acordo com o que já tinha
+               -- else (Error ("lambda tem copias de parametros"), s, e)
+      )
+
 eval env (List (Atom "define": args)) = maybe (define env args) (\v -> return v) (Map.lookup "define" env)
 -- lookup :: Ord k => k -> Map k a -> Maybe a
 -- maybe :: b -> (a -> b) -> Maybe a -> b
@@ -72,21 +103,56 @@ eval env (List (Atom func : args)) = mapM (eval env) args >>= apply env func
 eval env (Error s)  = return (Error s)
 eval env form = return (Error ("Could not eval the special form: " ++ (show form)))
 
--- lamBodyWithEnv :: 
-
 stateLookup :: StateT -> String -> StateTransformer LispVal
 stateLookup env var = ST $ 
-  (\s -> 
+  (\s e -> 
     (maybe (Error "variable does not exist.") 
-           id (Map.lookup var (union s env) 
-    ), s))
+           id (Map.lookup var (union env (union s e)) 
+    ), s, e))
 
--- recebe o estado do ambiente, a string let e retorna uma monad com as variaveis locais do let capturadas >>= (errada)
--- evalBack :: StateT -> String -> LispVal
-evalBack env = 
-            case (Map.lookup "let" env) of
-                Just (List elements) -> elements ++ (evalBack (newEnv env 0))
-                otherwise -> []
+--removeCopies :: (Eq a) => [a] -> [a]
+removeCopies [] = []
+removeCopies (x:xs) = x: removeCopies (Prelude.filter (\y -> (x /= y)) xs)
+
+-- evalBack :: StateT -> Int -> StateTransformer LispVal
+
+
+--  recebe o ambiente env e um index e retorna uma lista de variaveis do env presentes no ambiente e
+evalBack env index = ST $ 
+  (\s e -> 
+    (List (envVar env e index)
+    , s, e))
+
+-- funcao que retorna variaveis do env presentes no ambiente e
+
+envVar env e index = 
+  if (index < size env) then
+      case (retornaVars env index) of 
+          (Atom var) -> 
+              case (Map.lookup var e) of 
+                  Just (x) -> [x] ++ envVar env e (index+1) -- encontrou no ambiente e (local)
+                  otherwise -> envVar env e (index+1) -- nao encontrou no ambiente (busca o proximo elemento no env)
+          otherwise -> envVar env e (index+1) -- nao e uma variavel, busca o proximo elemento no env
+  else []
+
+   
+retornaVars env index = 
+  if (index < size env) then 
+        case snd (Map.elemAt index env) of
+            Just (Atom var) -> (Atom var)
+            otherwise -> retornaVars env (index+1)
+  else (Error "sem variavel")
+
+            
+ {-   (if (index < size e) then 
+        (case snd (Map.elemAt index e) of
+            Just (Native elements) -> (evalBack env (index+1))
+            otherwise ->  
+                case snd (Map.elemAt index env) of
+                    Just (Atom var) -> Atom var
+                    otherwise -> Atom [])
+    else Atom []), s, e)-}
+    
 
 -- newEnv :: StateT -> Int -> StateT
 newEnv env index = if ("let" == fst (Map.elemAt index env)) then (Map.deleteAt index env)
@@ -94,15 +160,33 @@ newEnv env index = if ("let" == fst (Map.elemAt index env)) then (Map.deleteAt i
 
 applySet :: StateT -> [LispVal] -> StateTransformer LispVal
 applySet env [(Atom id), val] = 
-  ST (\s -> let (ST f)    = eval env val
-                (result, newState) = f s
-              in 
-              if (member id newState) then (result, (insert id result newState))
-              else (Error "variable does not exist.", newState) 
+  ST (\s e -> let 
+                (ST f)    = eval env val
+                (result, newStateS, newStateE) = f s e
+            in 
+        if (member id newStateS) then (result, (insert id result newStateS), newStateE)                            
+        else if (member id newStateE) then (result, newStateS, (insert id result newStateE))
+        else (Error "variable does not exist.", newStateS, newStateE) 
+              
     )
 
 applySet env args = return (Error "wrong number of arguments") 
+
+-- aplica a funcao defineLet para inserir a variavel com seu valor no ambiente em seguida 
+-- ligação direta, direct bind >> com a avaliacao do corpo do let
+
+applyLet env bind@(List [Atom id, val]:[]) body = (defineLet env id val) >> eval env body
+applyLet env bind@(List [Atom id, val]:xs) body = (defineLet env id val) >> eval env body
+applyLet env list _ = ST (\s e -> (Error "not a let", s, e))
                 
+
+-- funcao de definicao de variaveis locais. Inserindo a variavel com valor avaliado no ambiente local
+defineLet env id val = 
+    ST (\s e -> let 
+                   (ST f) = eval env val
+                   (res, newStateS, newStateE) = f s e
+                   in (res, newStateS, (insert id res newStateE))
+       )
 
 --applyLet env [(Atom id), val] =                 
 
@@ -118,11 +202,11 @@ define env [(List [Atom id]), val] = defineVar env id val
 -- define env [(List l), val]                                       
 define env args = return (Error "wrong number of arguments")
 defineVar env id val = 
-  ST (\s -> let (ST f)    = eval env val
-                (result, newState) = f s
-            in (result, (insert id result newState))
+  ST (\s e -> let 
+                (ST f)    = eval env val
+                (result, newState, newStateE) = f s e
+            in  (result, (insert id result newState), e)
      )
-
 
 -- The maybe function yields a value of type b if the evaluation of 
 -- its third argument yields Nothing. In case it yields Just x, maybe
@@ -171,12 +255,11 @@ environment =
           $ insert "-"              (Native numericSub) 
           $ insert "/"              (Native numericDiv)
           $ insert "car"            (Native car)           
-          $ insert "cdr"            (Native cdr)
+          $ insert "cdr"            (Native cdr)   
+          $ insert "it?"            (Native predIt)
           $ insert "eq?"            (Native predEq)
           $ insert "fib"            (Native fib)
           $ insert "cons"           (Native cons)
-          $ insert "mod"            (Native numericMod)
-          $ insert "lt?"            (Native predLt)
             empty
 
 type StateT = Map String LispVal
@@ -187,13 +270,18 @@ type StateT = Map String LispVal
 -- because a StateTransformer gets the previous state of the interpreter 
 -- and, based on that state, performs a computation that might yield a modified
 -- state (a modification of the previous one). 
-data StateTransformer t = ST (StateT -> (t, StateT))
+
+
+-- StateTransformer foi modificado para incluir um novo StateT que representa os campos destinados ás variaveis locais
+-- definidas com let
+data StateTransformer t = ST (StateT -> StateT -> (t, StateT, StateT))
 
 instance Monad StateTransformer where
-  return x = ST (\s -> (x, s))
-  (>>=) (ST m) f = ST (\s -> let (v, newS) = m s
+  return x = ST (\s e -> (x, s, e))
+  (>>=) (ST m) f = ST (\s e -> let 
+                                 (v, newS, newE) = m s e
                                  (ST resF) = f v
-                             in  resF newS
+                             in  resF newS  newE
                       )
 
 
@@ -234,8 +322,8 @@ fibo n = n*(fibo (n-1))
 
 cons :: [LispVal] -> LispVal
 cons [a, List []] = List [a]
-cons [a, List as] = List $ a:as
-cons [a, DottedList b bs] = DottedList (a:b) bs
+cons [a, List as] = List $ [a] ++ as
+cons [a, DottedList b bs] = DottedList ([a] ++ b) bs
 cons [a, b] = DottedList [a] b
 
 car :: [LispVal] -> LispVal
@@ -264,11 +352,11 @@ predList (List _ : []) = Bool True
 predList (a:[]) = Bool False
 predList ls = Error "wrong number of arguments."
 
--- We are considering that (<) and (mod) are not associative.
-
-predLt :: [LispVal] -> LispVal
-predLt (Number n1:Number n2:[]) = Bool (n1 < n2)
-predLt _ = Error "wrong number of arguments"
+predIt :: [LispVal] -> LispVal
+predIt [] = Error "wrong number of arguments"
+predIt [l] = Error "wrong number of arguments"
+predIt (Number l:Number ls:[]) = Bool (l == ls)
+predIt l = Error "wrong number of arguments"
 
 predEq :: [LispVal] -> LispVal
 predEq lista@ (Bool _ : as: []) = eqExec lista
@@ -294,15 +382,12 @@ equal l = Error "wrong number of arguments"
 evalB :: (LispVal -> LispVal -> Bool) -> LispVal -> LispVal -> Bool
 evalB op l lx = op l lx
 
-containsZero :: [Integer] -> Bool
-containsZero [] = False
-containsZero (x:xs) | (x == 0) = True
-                    | otherwise = containsZero xs
-
--- Division is left-associative
+-- Defined such as operation div in haskell
 numericDiv :: [LispVal] -> LispVal
-numericDiv [] = Number 1
-numericDiv l = numericBinOpDiv div l
+numericDiv [] = Error "wrong number of arguments"
+numericDiv [l] = Error "wrong number of arguments"
+numericDiv (Number l:Number ls:[]) =  Number (div l ls)
+numericDiv l = Error "wrong number of arguments"
 
 numericSum :: [LispVal] -> LispVal
 numericSum [] = Number 0
@@ -320,10 +405,6 @@ numericSub [x] = if onlyNumbers [x]
                  else Error "not a number."
 numericSub l = numericBinOp (-) l
 
-numericMod :: [LispVal] -> LispVal
-numericMod (Number a:Number b:[]) = if b == 0 then Error "cannot divide by zero."
-                                    else (Number (mod a b))
-
 -- We have not implemented division. Also, notice that we have not 
 -- addressed floating-point numbers.
 
@@ -331,13 +412,6 @@ numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinOp op args = if onlyNumbers args 
                        then Number $ foldl1 op $ Prelude.map unpackNum args 
                        else Error "not a number."
-
-numericBinOpDiv :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
-numericBinOpDiv op args = if onlyNumbers args 
-                          then let numbers = Prelude.map unpackNum args
-                               in if containsZero $ tail numbers then Error "cannot divide by zero."
-                                  else Number $ foldl1 op numbers
-                          else Error "not a number."
                        
 onlyNumbers :: [LispVal] -> Bool
 onlyNumbers [] = True
@@ -356,20 +430,19 @@ unpackNum (Number n) = n
 comandoValido :: LispVal -> Bool
 comandoValido (List ((Atom "comment"):xs)) = False
 comandoValido _ = True
-
 filtrarComentarios :: LispVal -> LispVal
 filtrarComentarios (List l) = (List (Prelude.map filtrarComentarios $ Prelude.filter comandoValido l))
 filtrarComentarios lv = lv
 
-showResult :: (LispVal, StateT) -> String
-showResult (val, defs) = show val ++ "\n" ++ show (toList defs) ++ "\n"
+showResult :: (LispVal, StateT, StateT) -> String
+showResult (val, defs, lets) = show val ++ "\n" ++ show (toList defs) ++ "\n"
 
-getResult :: StateTransformer LispVal -> (LispVal, StateT)
-getResult (ST f) = f empty -- we start with an empty state. 
+getResult :: StateTransformer LispVal -> (LispVal, StateT, StateT)
+getResult (ST f) = f environment empty -- we start with an empty state. 
 
 main :: IO ()
 main = do args <- getArgs
-          putStr $ showResult $ getResult $ eval environment $ filtrarComentarios $ readExpr $ concat $ args 
+          putStr $ showResult $ getResult $ eval environment $ filtrarComentarios $ readExpr $ concat $ args
           
 
 
